ed6fe6191095eba1266c2f4795a033836daf8c47
danlooo <loosdaniel@gmail.com>
2020-12-18 15:26:41 +0100

    Refactor print functions

--- R/coabundance.R
+++ R/coabundance.R
@@ -2,6 +2,7 @@
 #' @param cor_res result object originally describing the relationships (e.g. object of class `rcorr`)
 #' @param edges tibble with columns from and to describing edges
 #' @param method character of correlation method used
+#' @export
 coabundance <- function(cor_res, edges, nodes = NULL, method = NULL, ...) {
   if (!is.null(nodes)) {
     taxa <- edges$from %>%
@@ -18,19 +19,34 @@ coabundance <- function(cor_res, edges, nodes = NULL, method = NULL, ...) {
   res
 }
 
-as_coabundance.spiec_easi_sparcc_res <- function(cor_res, ...) {
-  taxa <- cor_res$boot$data %>% colnames()
+cat_subtle <- function(...) cat(pillar::style_subtle(paste0(...)))
 
-  edges <-
-    tidyr::expand_grid(from = taxa, to = taxa) %>%
+
+print.coabundance <- function(x) {
+  n_interactions <- igraph::gsize(x$graph)
+  n_nodes <- igraph::gorder(x$graph)
+  
+  cat_subtle(str_glue("A coabundance object:\n   method: {x$method}\n   interactions: {n_interactions}\n   nodes: {n_nodes}\n"))
+}
+
+
+expand_nodes <- function(nodes) {
+  tidyr::expand_grid(from = nodes, to = nodes) %>%
     dplyr::mutate(comp = from %>% map2_chr(to, ~ c(.x, .y) %>%
-      sort() %>%
-      paste0(collapse = ""))) %>%
+                                             sort() %>%
+                                             paste0(collapse = ""))) %>%
     dplyr::group_by(comp) %>%
     dplyr::slice(1) %>%
     dplyr::filter(from != to) %>%
     dplyr::ungroup() %>%
-    dplyr::select(-comp) %>%
+    dplyr::select(-comp) 
+}
+
+as_coabundance.spiec_easi_sparcc_res <- function(cor_res, ...) {
+  taxa <- cor_res$boot$data %>% colnames()
+
+  edges <-
+    expand_nodes(taxa) %>%
     dplyr::mutate(
       estimate = sparcc_pval$cors,
       p.value = sparcc_pval$pvals,
@@ -64,35 +80,44 @@ as_coabundance.pulsar.refit <- function(cor_res, nodes = NULL, method = NULL) {
 
   edges <-
     cor_res %>%
-    getOptBeta() %>%
+    SpiecEasi::getOptCov() %>%
     as.matrix() %>%
-    as_tibble(rownames = "from") %>%
-    pivot_longer(-one_of("from"), names_to = "to", values_to = "estimate") %>%
-    mutate(to = to %>% str_remove("^V")) %>%
+    tibble::as_tibble(rownames = "from") %>%
+    tidyr::pivot_longer(-one_of("from"), names_to = "to", values_to = "estimate") %>%
+    dplyr::mutate(to = to %>% str_remove("^V")) %>%
     readr::type_convert(col_types = cols(from = col_integer(), to = col_integer(), estimate = col_double()))
 
   graph <-
     cor_res %>%
-    getRefit() %>%
-    adj2igraph() %>%
-    as_tbl_graph() %>%
-    mutate(taxon = used_taxa) %>%
-    activate(edges) %>%
-    left_join(edges, by = c("from", "to")) %>%
-    activate(nodes)
+    SpiecEasi::getRefit() %>%
+    SpiecEasi::adj2igraph() %>%
+    tidygraph::as_tbl_graph() %>%
+    tidygraph::mutate(taxon = used_taxa) %>%
+    tidygraph::activate(edges) %>%
+    dplyr::left_join(edges, by = c("from", "to")) %>%
+    tidygraph::activate(nodes)
 
   cur_nodes <-
     graph %>%
-    activate(nodes) %>%
-    as_tibble()
+    tidygraph::activate(nodes) %>%
+    tibble::as_tibble()
+  
+  zero_edges <-
+    expand_nodes(used_taxa) %>%
+    mutate(estimate = 0)
 
   edges <-
     graph %>%
-    activate(edges) %>%
-    as_tibble() %>%
-    left_join(cur_nodes %>% dplyr::rename(from_taxon = taxon), by = c("from" = "name")) %>%
-    left_join(cur_nodes %>% dplyr::rename(to_taxon = taxon), by = c("to" = "name")) %>%
-    dplyr::select(from = from_taxon, to = to_taxon, estimate)
+    tidygraph::activate(edges) %>%
+    tibble::as_tibble() %>%
+    dplyr::left_join(cur_nodes %>% dplyr::rename(from_taxon = taxon), by = c("from" = "name")) %>%
+    dplyr::left_join(cur_nodes %>% dplyr::rename(to_taxon = taxon), by = c("to" = "name")) %>%
+    dplyr::select(from = from_taxon, to = to_taxon, estimate) %>%
+    bind_rows(zero_edges) %>%
+    group_by(from, to) %>%
+    arrange(- abs(estimate)) %>%
+    slice(1) %>%
+    ungroup()
 
   coabundance(cor_res = cor_res, edges = edges, nodes = nodes, method = method)
 }
@@ -118,6 +143,9 @@ as_coabundance.default <- function(cor_res, edges, nodes = NULL, method = NULL, 
 }
 
 #' Convert coabundance objects
+#' 
+#' @param x object to transform to class coabundance. Supported classes: `pulsar.refit`, `tbl_df`, `rcorr`, `spiec_easi_sparcc_res`
+#' @export
 as_coabundance <- function(x, ...) {
   UseMethod("as_coabundance")
 }
